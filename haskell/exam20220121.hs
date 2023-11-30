73757d756243d1b49fa01aa941c36ff26a8a31d5
carloronconi <carlo.ronconi@mail.polimi.it>
2023-11-30 18:58:34 +0100

    Finish exam exercise with help of solution

--- haskell/exam20220121.hs
+++ haskell/exam20220121.hs
@@ -17,4 +17,23 @@ fmap f (Tv x y) = Tv (f x) (f y)
 fmap f (Tl lx ly) = Tl (fmap f lx) (fmap f ly)
 -- all good
 
+-- instance Applicative Tvtl where
+-- pure x y = Tv x y
+-- example usage: Tv (+1) (*2) <*> (Tv 1 2) = (Tv (Tv (+1 1) (+1 2)) (Tv (+2 1) (+2 2))
+-- (Tv fx fy) <*> (Tv x y) = (Tv ) -- no actually read theory slides to use same strategy as lists (with concatMap) ALL WRONG
+
+-- follow concatMap for lists pattern: first define something like concat (here with 3 +'s) for all cases
+(Tv x y) +++ (Tv z w) = Tl [x,y] [y,w]
+(Tv x y) +++ (Tl l r) = Tl (x:l) (y:r)
+(Tl l r) +++ (Tv x y) = Tl (l++[x]) (r++[y])
+(Tl l r) +++ (Tl x y) = Tl (l++x) (r++y)
+
+-- then use exactly the same defs as concatMap for lists, choosing (Tl [][]) as the "empty" (in lists it is [])
+tvtlconcat t = foldr (+++) (Tl [][]) t
+tvtlcmap f t = tvtlconcat $ fmap f t
+
+-- finally follow exactly same defs as concatMap for lists, choosing pure as (Tl [x] []) as impossible to give single value to Tv
+instance Applicative Tvtl where
+pure x = Tl [x] []
+x <*> y = tvtlcmap (\f -> fmap f y) x
 
