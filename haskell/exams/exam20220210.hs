0fef4c31f506f2cd884ade5626445d2dbaf4d27c
carloronconi <carlo.ronconi@mail.polimi.it>
2023-12-08 16:26:47 +0100

    Progress and decide to re-do

--- haskell/exams/exam20220210.hs
+++ haskell/exams/exam20220210.hs
@@ -3,21 +3,38 @@
   --1. Define the Gtree data structure.
   --2. Define gtree2list, i.e. a function which translates a Gtree to a list.
   --3. Make Gtree an instance of Functor, Foldable, and Applicative.
-
-module Exam where
+import Prelude
 
 -- 1.
-data Gtree a = Empty | Leaf a | Branch (a) ([Gtree a]) deriving Show
+data Gtree a = Empty | Leaf a | Branch (a) ([Gtree a]) deriving Show -- could have been made simpler by avoiding Leaf
+-- as you can just create a branch with an empty list
 
 -- 2.
 gtree2list :: Gtree a -> [a]
 gtree2list (Empty) = []
 gtree2list (Leaf a) = [a]
-gtree2list (Branch val lst) = [val] ++ (lst >>= gtree2list) -- bind for lists is concatMap
+gtree2list (Branch val lst) = [val] ++ (lst >>= gtree2list) -- bind for lists is concatMap, sol uses concatMap so you can use that!!
 
 -- 3.
 instance Foldable Gtree where
+foldr f z (Empty) = Empty
+foldr f z (Leaf a) = f z a
+-- foldr f z (Branch val lst) = f val $ foldr f z lst -- in this last foldr the lst is :: [Gtree a] so sol uses gtree2list
+foldr f z gtree = Prelude.foldr f z $ gtree2list gtree
 
+instance Functor Gtree where
+fmap f (Empty) = Empty
+fmap f (Leaf a) = Leaf $ f a
+-- fmap f (Branch val lst) = Branch (f val) (fmap f lst) -- should be (fmap (fmap f) lst), not sure why but with my solution type clash between Gtree b and b and viceversa
+fmap f (Branch val lst) = Branch (f val) (Prelude.fmap (Prelude.fmap f) lst)
+
+(+++) :: Gtree a -> Gtree a -> Gtree a
+Empty +++ Empty = Empty
+Empty +++ t = t
+t +++ Empty = t
+-- TODO: here becomes complex to consider case with leaf: re-do exercise with correct data constructor
+
+instance Applicative Gtree where
 
 
 -- testing it:
