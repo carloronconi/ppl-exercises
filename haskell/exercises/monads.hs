6ad4b2877bf9711ae4c85b0dbb394c73ee5a9838
carloronconi <carlo.ronconi@mail.polimi.it>
2023-12-09 15:46:11 +0100

    Add examples

--- haskell/exercises/monads.hs
+++ haskell/exercises/monads.hs
@@ -1,4 +1,5 @@
-module Mon where
+import Control.Monad.State
+--module Mon where
 
 result = Just 1 >>= Just >> Just 3 >> Nothing >> Just 6
 
@@ -11,3 +12,24 @@ putStrLn $ "You told me \"" ++ thing ++ "\".";
 }
 
 main2 = putStr "Please daddy tell me something> " >> getLine >>= \l -> putStrLn $ "You told me \"" ++ l ++ "\"."
+
+exmon :: (Monad m, Num r) => m r -> m r -> m r
+exmon m1 m2 = do
+  x <- m1
+  y <- m2
+  return $ x-y
+
+lst = exmon [10, 11] [1, 7]
+
+strange = exmon (do
+  putStr "?> "
+  x <- getLine
+  return (read x :: Int))
+  (return 10)
+
+runStateM (StateT f) st = f st
+
+ex = runStateM (do
+  x <- return 5
+  return (x+1))
+  333
