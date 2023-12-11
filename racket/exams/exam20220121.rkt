7eee6472d2c6adf5ac9aafc9e331c4051ac736c9
carloronconi <carlo.ronconi@mail.polimi.it>
2023-12-11 09:53:53 +0100

    Solve Racket exercise on macros

--- racket/exams/exam20220121.rkt
+++ racket/exams/exam20220121.rkt
@@ -17,6 +17,20 @@
 ; 10
 ; 6
 
-(define banana 4)
+(define-syntax block
+  (syntax-rules (then were <-)
+    ((block (body-l...) then (body-r...) where (var <- l r)...)
+     (begin
+        (let ((var l)...)
+          body-l...)
+        (let((var r)...)
+          body-r...)))))
 
-(display banana)
+(block
+   ((displayln (+ x y))
+   (displayln (* x y))
+   (displayln (* z z)))
+   then
+   ((displayln (+ x y))
+   (displayln (* z x)))
+   where (x <- 12 3)(y <- 8 7)(z <- 3 2))
