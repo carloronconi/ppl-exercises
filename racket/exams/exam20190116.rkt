a877551562bd92a7480c5afda1878caa97ee82bf
carloronconi <carlo.ronconi@mail.polimi.it>
2023-12-12 15:46:18 +0100

    Solve exam with help of solution

---
+++ racket/exams/exam20190116.rkt
@@ -0,0 +1,37 @@
+#lang racket
+(define (inv x . rest)
+  (let ((lst (cons x rest)))
+    (foldl cons '() lst)))
+
+(define lst '(1 2 3 4))
+
+(inv 1 2 3 4 5)
+(inv 1)
+
+#|
+I misunderstood assignment, let's try again
+couldn't figure out how to concat an element with a list, as cons "flattens" the list.
+Suggestion note:
+> (list 1 2 '(3))
+'(1 2 (3))
+> (cons 1 '(2))
+'(1 2)
+|#
+
+(define (func . lst)
+  (foldl list
+         (foldl cons '() lst)
+         lst))
+
+#|
+solution by prof: same but uselessly re-defines the function list as a lambda
+|#
+
+(define (f . L)
+  (foldl (lambda (x y)
+           (list x y))
+         (foldl cons '() L)
+         L))
+
+(f 3 4 5)
+(f)
