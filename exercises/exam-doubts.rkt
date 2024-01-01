e08fae7f5eb95c21dbeeca554c85e971a1aca69e
carloronconi <carlo.ronconi@mail.polimi.it>
2024-01-01 17:44:40 +0100

    Add doubts

--- exercises/exam-doubts.rkt
+++ exercises/exam-doubts.rkt
@@ -8,3 +8,39 @@
 (foldl show-sum
        0
        numbers)
+
+(define (x . y) y)
+
+(x 1 2 3)
+(x)
+(x '(1 2 3))
+
+(define (x2 y) y)
+
+; (x2 1 2 3) error number of arguments: found 3 but expected 1
+(x2 1)
+
+(define (x3 a b . c)
+  (begin
+    (display a)
+    (newline)
+    (display b)
+    (newline)
+    (display c)
+    (newline)))
+
+;(x3 1) error number of arguments: found 1 but expected at least 2
+(x3 1 2)
+(x3 1 2 3)
+(x3 1 2 3 4)
+
+(define (test . arg)
+  (let ((x (car arg))
+        (y (cdr arg)))
+    (display x)
+    (newline)
+    (display y)
+    (newline)))
+
+(newline)
+(test 1 2 3)
